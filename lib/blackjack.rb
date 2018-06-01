870eacc984ac32bdcae77f8101ab01dbe61477ef
Choeles Tung <choelestung@gmail.com>
2018-06-01 16:45:54 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -59,8 +59,8 @@ def runner
   welcome
   card_total = initial_round
   until card_total < 21
-    card_total = hit? 
-    card_total =
+    card_total = hit?(card_total)
+    card_total = 
   end
   end_game
 
